f7b1bb9e34c0861be732d95d7d4c725aa51c4f3d
Valentin <valrub@gmail.com>
2018-04-10 12:10:39 +0300

    String MAnipulations changes

--- MS/WF_SNIPPETS/StringManipulations.js
+++ MS/WF_SNIPPETS/StringManipulations.js
@@ -9,8 +9,13 @@ ans = inp.split('').filter(el => /[A-Za-z0-9]/.test(el)).join('');
 console.log(ans);
 
 // -----------------------------------------------------------------------------------------------
-
-
+var sourceString = "#ГРЕ ФФ";
+// Example with \w
+var rs = sourceString.replace(/[^\w\s]/gi, '');
+console.log(rs + ' 1');
+// Example listing the specific chars we want to delete
+var r = sourceString.replace(/[`~!@#$%^&*()_|+\-=÷¿?;:'",.<>\{\}\[\]\\\/]/gi, '');
+console.log(r + ' 2');
 // -----------------------------------------------------------------------------------------------
 
 
